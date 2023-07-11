8aef045ccf0fb271f0a59cff3b0391ab5584eec1
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-11 16:08:14 -0500

    Clean data

--- Scripts/species abundance figures.R
+++ Scripts/species abundance figures.R
@@ -3,6 +3,14 @@
 # I added a sheet in excel that contained only the sauger lengths
 # The sauger data could probably be isolated within R, but this was easier for now
 
+#  Load in data
+data = readxl::read_excel("Data/JoeMasterData.xlsx")
+
+# Clean data
+Joe_Cool_water_fishes_data_Joe = data %>%
+  filter(Species == "SAR") %>%
+  rename(SAR = `TL (mm)`)
+
 #########Placing the data into size bins###################################
 # seq(min value, max value, bin sizes)
 Joe_Cool_water_fishes_data_Joe = mutate(Joe_Cool_water_fishes_data_Joe, 
@@ -15,17 +23,3 @@ ggplot(Joe_Cool_water_fishes_data_Joe, aes(x=SAR_bins))+
                                        xlab("Length Bins (mm)")+
                                        ylab("Frequency")+
                                        ggtitle("Sauger")
-###########################################################################
-
-# Fish added to master, this one is the up to date figure
-
-library(ggplot2)
-
-JoeMasterData = mutate(JoeMasterData, bins=cut(SARTL, breaks=seq(0,800,100))) 
-
-ggplot(JoeMasterData, aes(x=bins))+
-                      geom_bar()+
-                      xlab("Total Length Bins (mm)")+
-                      ylab("Freqiency")+
-                      ggtitle("Sauger (SAR)") 
-  
