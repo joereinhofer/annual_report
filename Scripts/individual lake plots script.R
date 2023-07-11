cd585549a80923ae4d4719e7217c7fbb0c04e686
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-11 16:31:37 -0500

    Clean data

--- Scripts/individual lake plots script.R
+++ Scripts/individual lake plots script.R
@@ -3,19 +3,18 @@
 # Data started as a column for each year with the lengths of individual fish in each year column. #
 # Starting excel file = Joe_Cool_water_fishes_data_Joe
 # Renamed in code below to "Decatur" because that was the lake used and it was a shorter name.
-
-Decatur <- Joe_Cool_water_fishes_data_Joe
- 
-###### Converting to long form with year and length linked together###########
-# Instead of years just being column names, each length has its associated year in a cell next to it. 
-# Converts columns to just year and length
 library(tidyverse)
-pivot_longer(Decatur, colnames(Decatur))
 
-longformdata <- pivot_longer(Decatur, colnames(Decatur))
-# Named the new dataframe "longformdata" 
+# Load in data
+data = readxl::read_excel("Data/JoeMasterData.xlsx")
 
-colnames(longformdata) = c("year", "lengths")
+# Clean data
+DecaturWAE = data %>%
+  filter(Species == "WAE", `Water Body`== "Decatur") %>%
+  mutate(`Collection Date` = format(as.Date(`Collection Date`, format="%Y-%m-%d"),"%Y")) %>%
+  select(`Collection Date`, `TL (mm)`)
+
+colnames(DecaturWAE) = c("year", "lengths")
 # Column names became "year" and "lenghts", will be referenced in later arguments.
 #################################################################
 
@@ -24,39 +23,14 @@ colnames(longformdata) = c("year", "lengths")
 # seq(min value, max value, bin sizes)
 # Naming the x axis bins "sizeclass"
 # %>% and "remove_missing" used to remove the N/A's, so they don't show up in figure. 
-lengths = longformdata %>% 
+lengths = DecaturWAE %>% 
                  mutate(sizeclass = cut(lengths, breaks = seq(100, 800, 100)))%>%
                  remove_missing()
 #######################################################################
 
 
-###########Plotting###############################################
-# fill=year is to fill the bars different colors based on the year.
-# position=position_dodge is what makes the grouped bars in each bin.
-ggplot(lengths, aes(x=sizeclass, fill= year))+
-  geom_bar(position = position_dodge())
-#################################################################
+# PLOTTING
+ggplot(lengths, aes(x=sizeclass, fill= year))+  # fill=year is to fill the bars different colors based on the year.
+  geom_bar(position = position_dodge())  # position=position_dodge is what makes the grouped bars in each bin.
 
-
-
-
-# FISH ADDED TO MASTER, THIS IS THE UP TO DATE PLOT
-
-Decatur <- JoeMasterData
-
-library(tidyverse)
-pivot_longer(Decatur, colnames(Decatur))
-
-longform <- pivot_longer(Decatur, colnames(Decatur))
-
-colnames(longform) = c("year", "TL")
-
-TL = longform %>%
-       mutate(sizebins = cut(TL, breaks = seq(0,800,100)))%>%
-       remove_missing()
-
-ggplot(TL, aes(x=sizebins, fill=year))+
-          geom_bar(position=position_dodge())+
-           ggtitle("Decatur")+
-          xlab("Size Bins")+ ylab("Frequency")
 
