d42be2dd686326e468613582aae8ff965ca7446a
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-13 14:47:20 -0500

    use function for length at age

--- Scripts/length at age plots.R
+++ Scripts/length at age plots.R
@@ -19,13 +19,25 @@ ggplot(age_at_length_figures_for_lakes, aes(x=`WS FINAL AGE`, y=`TL (mm)`))+
                       xlab("Age (Whole Spine)") + ylab("Total Length (mm)")+  # note in report they are WS ages
                      scale_x_continuous(breaks =  seq(0, 8, 1))+
                      scale_y_continuous(breaks =  seq(0, 750, 50))+
-                     theme_bw()+ 
+                     theme_classic()+ 
                      theme(plot.title = element_text(hjust = 0.5))
   
 #############################################
 
 
 
+library(ggplot2)
+library(tidyverse)
+source("functions/plot_age_at_length.R")
+
+# Load in data
+data = readxl::read_excel("Data/JoeMasterData.xlsx")
+
+# Clean data
+age_at_length_figures_for_lakes = data %>%
+  filter(`Water Body` == "Decatur", Species == "WAE")
+
+plot_age_at_length(age_at_length_figures_for_lakes)
 
           
           
