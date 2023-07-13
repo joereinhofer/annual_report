df28a07f67c7804809f705ccd759ae862ada916e
Reinhofer <jpr4@illinois.edu>
2023-07-13 11:49:02 -0500

    Change look of length at age plot

--- Scripts/length at age plots.R
+++ Scripts/length at age plots.R
@@ -1,6 +1,6 @@
 # LENGTH AT AGE PLOTS
 # Using whole spine ages 
-# data was two columns, whole spine final age (wsfage) and length (tl) 
+
 
 # DECATUR ########
 library(ggplot2)
@@ -14,11 +14,14 @@ age_at_length_figures_for_lakes = data %>%
   filter(`Water Body` == "Decatur", Species == "WAE")
 
 ggplot(age_at_length_figures_for_lakes, aes(x=`WS FINAL AGE`, y=`TL (mm)`))+
-        geom_point()+ geom_smooth(se = FALSE) +
-        ggtitle("Lake Decatur")+
-        xlab("Age") + ylab("Total Length (mm)")+  # note in report they are WS ages
-        theme(plot.title = element_text(hjust = 0.5))+
-  scale_x_continuous(breaks =  seq(0, 8, 1))
+                      geom_point()+ geom_smooth(se = FALSE) +
+                      ggtitle("Lake Decatur Walleye")+
+                      xlab("Age (Whole Spine)") + ylab("Total Length (mm)")+  # note in report they are WS ages
+                     scale_x_continuous(breaks =  seq(0, 8, 1))+
+                     scale_y_continuous(breaks =  seq(0, 750, 50))+
+                     theme_bw()+ 
+                     theme(plot.title = element_text(hjust = 0.5))
+  
 #############################################
 
 
