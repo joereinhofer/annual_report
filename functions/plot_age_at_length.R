73183037ce396076ac84cf8f233c4dfa451d18cb
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-14 08:00:54 -0500

    save function

--- functions/plot_age_at_length.R
+++ functions/plot_age_at_length.R
@@ -1,7 +1,7 @@
-plot_age_at_length = function(data){
+plot_age_at_length = function(data, title){
   ggplot(data, aes(x=`WS FINAL AGE`, y=`TL (mm)`))+
     geom_point()+ geom_smooth(se = FALSE) +
-    ggtitle("Lake Decatur Walleye")+
+    ggtitle(title)+
     xlab("Age (Whole Spine)") + ylab("Total Length (mm)")+  # note in report they are WS ages
     scale_x_continuous(breaks =  seq(0, max(na.omit(data$`WS FINAL AGE`)), 1))+
     scale_y_continuous(breaks =  seq(0, max(na.omit(data$`TL (mm)`)), 50))+
