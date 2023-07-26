1097a8974bbffe9c1cae232173d10b8b3dbd4aef
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-26 17:32:23 -0500

    Work on script

--- Scripts/age distribution.R
+++ Scripts/age distribution.R
@@ -4,23 +4,54 @@
 
 library(dplyr)
 library(ggplot2)
+library(plotrix)
 
 readxl::read_excel("Data/JoeMasterData.xlsx")
 
+data = readxl::read_excel("Data/JoeMasterData.xlsx", 
+                          col_types = c("numeric", "numeric", "text", 
+                                        "text", "text", "text", "text", "text", 
+                                        "numeric", "numeric", "text", "text", 
+                                        "numeric", "text", "numeric", "text", 
+                                        "text", "numeric", "numeric", "text", 
+                                        "numeric", "numeric", "text", "text", 
+                                        "numeric", "text", "text", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "text", "numeric", "text", "text", 
+                                        "numeric", "text", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "numeric", 
+                                        "numeric", "numeric", "text", "numeric", 
+                                        "numeric"))
+
 # Clean data
-age_distribution = data %>%
+ages = data %>%
    filter(Species == "WAE", `Water Body` == "Decatur")%>%
-   select('WS FINAL AGE')
+   select('WS FINAL AGE')%>%
+   remove_missing()
 
-age_distribution = mutate('WS FINAL AGE' = as.numeric('WS FINAL AGE'))
-
-summary(age_distribution$`WS FINAL AGE`)
-# Mean = 2.4 mm
+summary(ages)
+std.error(ages)
+sd(ages)
+# Mean = 2.4 ± 0.11 yrs
 # Range = 0-8 yrs
+# SE = 0.11
 
 
-ggplot(age_distribution, aes(x='WS FINAL AGE'))+
-      labs(title = "Lake Decatur", )
+ggplot(ages, aes(x='WS FINAL AGE'))+
+       geom_bar()+
+      labs(title = "Lake Decatur", caption = "Mean = 2.4 ± 0.11 yrs, Range = 0-8 yrs")+
+     theme(plot.title = element_text(hjust = 0.5))
+      theme_classic()+
+      xlab("Age (yrs)")+ ylab("Frequency")
+      
+      
   
   
  
