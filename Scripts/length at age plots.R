9ab78a65e981dd6a6d7c7e83c9557f61165b260c
Joe Reinhofer <reinhofer.joe@gmail.com>
2023-07-27 16:35:40 -0500

    Add confidence and sauger figure

--- Scripts/length at age plots.R
+++ Scripts/length at age plots.R
@@ -11,13 +11,13 @@ data = readxl::read_excel("Data/JoeMasterData.xlsx")
 
 # Clean data
 age_at_length_figures_for_lakes = data %>%
-  filter(`Water Body` == "Decatur", Species == "WAE")
+  filter(`Water Body` == "Shelbyville", Species == "SAR")
 
 ggplot(age_at_length_figures_for_lakes, aes(x=`WS FINAL AGE`, y=`TL (mm)`))+
-                      geom_point()+ geom_smooth(se = FALSE) +
-                      ggtitle("Lake Decatur Walleye")+
+                      geom_point()+ geom_smooth() +
+                      ggtitle("Lake Shelbyville")+
                       xlab("Age (Whole Spine)") + ylab("Total Length (mm)")+  # note in report they are WS ages
-                     scale_x_continuous(breaks =  seq(0, 8, 1))+
+                     scale_x_continuous(breaks =  seq(0, 11, 1))+
                      scale_y_continuous(breaks =  seq(0, 750, 50))+
                      theme_classic()+ 
                      theme(plot.title = element_text(hjust = 0.5))
